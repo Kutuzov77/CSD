bd7e6c35e3529e054f94fb22a783896883eb3924
Kutuzov77 <sarthak.bhatt@credextechnology.com>
2023-07-18 15:33:15 +0530

    added routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import Login from './Login/Login';
+import Header from './Components/Common/Header/NavScrollExample';
+import {BrowserRouter, Route,Routes} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <Login/>
+       <BrowserRouter>
+       {/* <Header/> */}
+    <Routes>
+      <Route exact path='/' element={<Login/>}></Route>
+    </Routes>
+    
+    </BrowserRouter>
+            
+    
     </div>
   );
 }
