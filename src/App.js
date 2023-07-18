dca0816f3644b79914d382241972cf12d3355b51
Kutuzov77 <sarthak.bhatt@credextechnology.com>
2023-07-18 15:56:56 +0530

    yy

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Login from './pages/Login/Login';
-import Header from './Components/Common/Header/NavScrollExample';
+import NavScrollExample from './components/Common/Header/NavScrollExample.jsx'
 import {BrowserRouter, Route,Routes} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
        <BrowserRouter>
-       {/* <Header/> */}
+     <NavScrollExample/>
     <Routes>
       <Route exact path='/' element={<Login/>}></Route>
     </Routes>
